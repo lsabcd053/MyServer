34ed874eef9a75fd72c03501c63e91698689e0b8
lsabcd <lidingbo_1989@163.com>
2016-08-03 09:00:58 +0800

    init

---
+++ server.py
@@ -0,0 +1,23 @@
+import BaseHTTPServer
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+    Page = '''\
+    <html>
+    <body>
+    <p>Hello, web!</p>
+    </body>
+    </html>
+    '''
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type","text/html")
+        self.send_header("Content-Length",str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
+
+if __name__ == "__main__":
+    serverAddress = ('',8899)
+    server = BaseHTTPServer.HTTPServer(serverAddress,RequestHandler)
+    server.serve_forever()
