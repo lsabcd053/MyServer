4acde3a36a1d2e5a29de998e56fe8b624523ddab
lsabcd <lidingbo_1989@163.com>
2016-08-04 10:43:19 +0800

    返回文件内容

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
 import BaseHTTPServer
+import os
+
+class ServerException(Exception):
+    def __init__(self,msg):
+        Exception.__init__(self,msg)
+        self.msg = msg
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
@@ -16,6 +22,15 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 </body>
 </html>
 '''
+
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
 
     def create_page(self):
         value = {
@@ -35,10 +50,43 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(page)
 
-    def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+    # def do_GET(self):
+    #     page = self.create_page()
+    #     self.send_page(page)
 
+    def do_GET(self):
+        try:
+            full_path = os.getcwd() + self.path
+
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unknow object '{0}'".format(self.path))
+
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_file(self,full_path):
+        try:
+            with open(full_path,'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path,msg)
+            self.handle_error()
+
+    def send_content(self,content,status=200):
+        self.send_response(status)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+
+    def handle_error(self,msg):
+        contnt = self.Error_Page.format(path=self.path,msg = msg)
+        self.send_content(contnt)
 
 if __name__ == "__main__":
     serverAddress = ('',8899)
